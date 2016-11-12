22ff9f1a6e62d23481824057fae1835d78a4060c
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2016-11-12 19:48:39 +0100

    add username handling

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,17 @@
-console.log('Game will be here!');
+const $playForm = document.getElementById('playForm');
+const $username = document.getElementById('username');
+
+$playForm.onsubmit = e => {
+  e.preventDefault();
+
+  const username = $username.value;
+
+  if (!username) {
+    $playForm.classList.add('shake');
+    setTimeout(() => $playForm.classList.remove('shake'), 1000);
+    return;
+  }
+
+  console.log('Game will start here with ' + username + '!');
+};
+
