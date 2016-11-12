ff72e36c5287d5f4f15bc95aefff9c07fedd2466
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2016-11-12 16:28:30 +0100

    add views

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+const path = require('path');
+const swig = require('swig');
 const express = require('express');
+const logger = require('morgan');
+
 const app = express();
 
 app.set('port', (process.env.PORT || 5000));
+
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, './views'));
+
+app.use(logger('dev'));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node Knockout 2016 Asteroids on Steroids game!');
+  res.render('base');
 });
 
 app.listen(app.get('port'), () => {
