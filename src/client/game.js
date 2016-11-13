6ad3f295c52946d38d32ecea985831807b01b833
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2016-11-13 15:11:39 +0100

    start game restructuring

--- src/client/game.js
+++ src/client/game.js
@@ -1,24 +1,20 @@
 const socket = require('./socket');
 
-module.exports = function runGame (elementId) {
+function runGame (elementId) {
+  const game = new Phaser.Game('100%', '100%', Phaser.CANVAS, elementId, { create, update });
+
   let player;
   let weapon;
   let cursors;
   let fireButton;
 
-  function create() {
-    this.world.setBounds(-1000, -1000, 2000, 2000);
-
-    // fullscreen and resizable
-    this.scale.scaleMode = Phaser.ScaleManager.RESIZE;
-    this.scale.pageAlignHorizontally = true;
-    this.scale.pageAlignVertically = true;
-
+  function createShipGraphics (type) {
     // Create our bitmapData which we'll use as a Sprite texture for the ship
-    let shipBMD = this.add.bitmapData(32, 32);
+    const shipBMD = game.add.bitmapData(32, 32);
+
     shipBMD.context.strokeStyle = 'white';
     shipBMD.context.lineWidth = 2;
-    shipBMD.context.fillStyle = 'black';
+    shipBMD.context.fillStyle = type === 'rival' ? 'green' : 'black';
     shipBMD.context.beginPath();
     shipBMD.context.moveTo(0, 4);
     shipBMD.context.lineTo(32, 16);
@@ -27,20 +23,40 @@ module.exports = function runGame (elementId) {
     shipBMD.context.closePath();
     shipBMD.context.fill();
     shipBMD.context.stroke();
-    this.cache.addBitmapData('ship', shipBMD); // Put the bitmapData into the cache
 
-    //  Create our bitmapData which we'll use as a Sprite texture for the bullets
-    let bulletBMD = this.add.bitmapData(32, 32);
+    game.cache.addBitmapData(`${type}Ship`, shipBMD);
+  }
+
+  function createBulletGraphics () {
+    // Create our bitmapData which we'll use as a Sprite texture for the bullets
+    const bulletBMD = game.add.bitmapData(32, 32);
+
     bulletBMD.context.strokeStyle ='white';
     bulletBMD.context.fillStyle ='white';
     bulletBMD.context.fillRect(0, 6, 32, 2);
     bulletBMD.context.fillRect(0, 24, 32, 2);
-    this.cache.addBitmapData('bullet', bulletBMD);
 
-    //  Creates 30 bullets, using the 'bullet' graphic
-    weapon = this.add.weapon(30, this.cache.getBitmapData('bullet'));
+    game.cache.addBitmapData('bullet', bulletBMD);
+  }
 
-    //  The bullets will be automatically killed when they are 2.5s old
+  function preload () {
+    // fullscreen and resizable
+    game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
+    game.scale.pageAlignHorizontally = true;
+    game.scale.pageAlignVertically = true;
+  }
+
+  function create() {
+    game.world.setBounds(-1000, -1000, 2000, 2000);
+
+    createShipGraphics('user');
+    createShipGraphics('rival');
+    createBulletGraphics();
+
+    // Creates 30 bullets, using the 'bullet' graphic
+    weapon = game.add.weapon(30, game.cache.getBitmapData('bullet'));
+
+    // The bullets will be automatically killed when they are 2.5s old
     weapon.bulletKillType = Phaser.Weapon.KILL_LIFESPAN;
     weapon.bulletLifespan = 2500;
 
@@ -53,14 +69,14 @@ module.exports = function runGame (elementId) {
     //  Wrap bullets around the world bounds to the opposite side
     weapon.bulletWorldWrap = true;
 
-    player = this.add.sprite(200, 150, this.cache.getBitmapData('ship'));
+    player = game.add.sprite(200, 150, game.cache.getBitmapData('userShip'));
 
     // follow player
-    this.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON);
+    game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON);
 
     player.anchor.set(0.5);
 
-    this.physics.arcade.enable(player);
+    game.physics.arcade.enable(player);
 
     player.body.drag.set(70);
     player.body.maxVelocity.set(200);
@@ -71,14 +87,13 @@ module.exports = function runGame (elementId) {
     weapon.trackSprite(player, 8, 0, true);
 
     // Controls setup
-    cursors = this.input.keyboard.createCursorKeys();
-    fireButton = this.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
-
+    cursors = game.input.keyboard.createCursorKeys();
+    fireButton = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
   }
 
   function update() {
     if (cursors.up.isDown) {
-      this.physics.arcade.accelerationFromRotation(player.rotation, 300, player.body.acceleration);
+      game.physics.arcade.accelerationFromRotation(player.rotation, 300, player.body.acceleration);
     } else {
       player.body.acceleration.set(0);
     }
@@ -97,8 +112,10 @@ module.exports = function runGame (elementId) {
       weapon.fire();
     }
 
-    this.world.wrap(player, 16);
+    game.world.wrap(player, 16);
   }
 
-  return new Phaser.Game('100%', '100%', Phaser.CANVAS, elementId, {create, update});
+  return game;
 };
+
+module.exports = runGame;
