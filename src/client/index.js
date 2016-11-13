1f802b793fba3695c0ddb6629b01e5605aa7282a
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2016-11-13 02:23:50 +0100

    restructure

--- src/client/index.js
+++ src/client/index.js
@@ -1,39 +1,13 @@
 const runGame = require('./game');
-const introAudio = new Audio('./assets/adropday-the_cougar.mp3');
-// const gameAudio = new Audio('./assets/uoki-toki-king_of_my_castle.mp3'); // use it when game starts
 const socket = require('./socket');
+const userInteracions = require('./ui');
 
-introAudio.play();
-introAudio.addEventListener('ended', function () {
-  introAudio.currentTime = 0;
-  introAudio.play();
-}, false);
-
-const $playForm = document.getElementById('playForm');
-const $username = document.getElementById('username');
-
-$playForm.onsubmit = function (e) {
-  e.preventDefault();
-
-  const username = $username.value;
-
-  if (!username) {
-    $playForm.classList.add('shake');
-    setTimeout(function () {
-      $playForm.classList.remove('shake');
-    }, 1000);
-    return;
-  }
-
-  introAudio.pause();
-  introAudio.currentTime = 0;
-
+userInteracions.onPlay(function (username) {
   socket.emit('new-user', { username });
-  socket.on('new-user', (user) => {
+  socket.on('new-user', function (user) {
     console.log(user);
   });
 
-  console.log('Game will start here with ' + username + '!');
-};
-
-runGame('game');
+  console.log('Run game');
+  runGame('game');
+});
---
+++ src/client/ui.js
@@ -0,0 +1,70 @@
+const store = require('store');
+
+const cachedSound = store.get('soundEnabled');
+let soundEnabled = typeof cachedSound !== 'boolean' ? true : cachedSound;
+let onPlayCallback = function noop () {};
+
+// elements
+const $noSound = document.getElementById('noSound');
+const $playForm = document.getElementById('playForm');
+const $username = document.getElementById('username');
+
+// audio
+const audio = document.createElement('audio');
+const introAudio = './assets/adropday-the_cougar.mp3';
+const gameAudio = './assets/adropday-hotline_ berlin.mp3';
+
+audio.src = introAudio;
+if (soundEnabled) {
+  audio.play();
+} else {
+  $noSound.classList.add('active');
+}
+// loop audio
+audio.addEventListener('ended', function () {
+  audio.currentTime = 0;
+  audio.play();
+}, false);
+
+// username form submit
+$playForm.onsubmit = function (e) {
+  e.preventDefault();
+
+  const username = $username.value;
+
+  if (!username) {
+    $playForm.classList.add('shake');
+    setTimeout(function () {
+      $playForm.classList.remove('shake');
+    }, 1000);
+    return;
+  }
+
+  if (soundEnabled) {
+    audio.pause();
+    audio.src = gameAudio
+    audio.play();
+  }
+
+  onPlayCallback(username);
+};
+
+// toggle audio
+$noSound.onclick = function (e) {
+  e.preventDefault();
+
+  soundEnabled = !soundEnabled;
+  store.set('soundEnabled', soundEnabled);
+
+  if (soundEnabled) {
+    $noSound.classList.remove('active');
+    audio.play();
+  } else {
+    $noSound.classList.add('active');
+    audio.pause();
+  }
+};
+
+exports.onPlay = function (callback) {
+  onPlayCallback = callback;
+};
